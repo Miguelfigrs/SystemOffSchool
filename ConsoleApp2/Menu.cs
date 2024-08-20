3fd65026378b0af7216fe9c41f4538be25258dd4
Miguel Figueiredo R. de Souza <miguelfigrs@gmail.com>
2024-08-20 00:35:30 -0300

    Mudança De nomesAdd project files.

---
+++ ConsoleApp2/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  class Menu
+    {
+        public int DisplayMenu(string[] options, string imagem)
+        {
+            int selected = 0;
+             
+
+
+            ConsoleKey key;
+            do
+            {
+
+                Console.Clear();
+                Console.WriteLine(imagem);
+                for (int i = 0; i < options.Length; i++)
+                {
+                    
+
+                    if (i == selected)
+                    {
+
+                        Console.WriteLine($"> {options[i]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($" {options[i]}");
+                    }
+                }
+                key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.UpArrow && selected > 0)
+                {
+                    selected--;
+                }
+                else if (key == ConsoleKey.DownArrow && selected < options.Length - 1)
+                {
+                    selected++;
+                }
+
+            } while (key != ConsoleKey.Enter);
+            return selected;
+        }
+    
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp2
+{
+
+    public class Program
+    {
+        public string Teacher(string teacher)
+        {
+            teacher = @"
+ ,--.--------.    ,----.   ,---.       _,.----.  ,--.-,,-,--,    ,----.               
+/==/,  -   , -\,-.--` , \.--.'  \    .' .' -   \/==/  /|=|  | ,-.--` , \  .-.,.---.   
+\==\.-.  - ,-./==|-  _.-`\==\-/\ \  /==/  ,  ,-'|==|_ ||=|, ||==|-  _.-` /==/  `   \  
+ `--`\==\- \  |==|   `.-./==/-|_\ | |==|-   |  .|==| ,|/=| _||==|   `.-.|==|-, .=., | 
+      \==\_ \/==/_ ,    /\==\,   - \|==|_   `-' \==|- `-' _ /==/_ ,    /|==|   '='  / 
+      |==|- ||==|    .-' /==/ -   ,||==|   _  , |==|  _     |==|    .-' |==|- ,   .'  
+      |==|, ||==|_  ,`-./==/-  /\ - \==\.       /==|   .-. ,\==|_  ,`-._|==|_  . ,'.  
+      /==/ -//==/ ,     |==\ _.\=\.-'`-.`.___.-'/==/, //=/  /==/ ,     //==/  /\ ,  ) 
+      `--`--``--`-----`` `--`                   `--`-' `-`--`--`-----`` `--`-`--`--'  
+";
+
+            return teacher;
+        }
+        static void PaginaDoProf()
+        {
+
+            string[] opcoesDoProf = { "Dados do Aluno", "Notas do Aluno", "Presença Do aluno" };
+            string imagem = "";
+            Menu menu = new Menu();
+            Program program = new Program();
+            string imagemProf = program.Teacher(imagem);
+
+            menu.DisplayMenu(opcoesDoProf, imagemProf);
+
+        }
+        static void Main(string[] args)
+        {
+            string[] options = { "Pesquisar aluno", "Sair"};
+            string artcii = "";
+
+            Menu menu = new Menu();
+            Program program = new Program();
+            
+            string teacherArt = program.Teacher(artcii);
+            int selected = menu.DisplayMenu(options,teacherArt);
+          
+
+            Console.WriteLine(menu);
+            switch (selected)
+            {
+                case 0:
+
+                    PaginaDoProf();
+
+                    break;
+                case 1:
+                    Console.WriteLine("Saindo...");
+                    Environment.Exit(0); 
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+
+            }
+        }
+    }
+}
